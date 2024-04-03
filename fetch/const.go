df24ae0d06adc41998e2571ed4ed58e1f8a065a4
Sokwva <1519181530@qq.com>
2024-04-03 18:28:31 +0800

    add urls

--- fetch/const.go
+++ fetch/const.go
@@ -11,8 +11,21 @@ var HEADERS = map[string]string{
 }
 
 const (
-	IndexUrl  = "https://www.acfun.cn/"
-	ApiSrcUrl = "https://www.acfun.cn/?pagelets=pagelet_header,pagelet_game,pagelet_douga,pagelet_amusement,pagelet_bangumi_list,pagelet_life,pagelet_tech,pagelet_dance,pagelet_music,pagelet_film,pagelet_fishpond,pagelet_sport&reqID=0&ajaxpipe=1"
+	IndexUrl          = "https://www.acfun.cn/"
+	BangumiIndexUrl   = "https://www.acfun.cn/v/list155/index.htm"
+	AnimateIndexUrl   = "https://www.acfun.cn/v/list1/index.htm"
+	MusicIndexUrl     = "https://www.acfun.cn/v/list58/index.htm"
+	EntertainIndexUrl = "https://www.acfun.cn/v/list60/index.htm"
+	DanceIndexUrl     = "https://www.acfun.cn/v/list123/index.htm"
+	GameIndexUrl      = "https://www.acfun.cn/v/list59/index.htm"
+	TechIndexUrl      = "https://www.acfun.cn/v/list70/index.htm"
+	MovieIndexUrl     = "https://www.acfun.cn/v/list68/index.htm"
+	SportIndexUrl     = "https://www.acfun.cn/v/list69/index.htm"
+	FishPondIndexUrl  = "https://www.acfun.cn/v/list125/index.htm"
+	ArticleIndexUrl   = "https://www.acfun.cn/v/list63/index.htm"
+
+	BillboardApiUrl = "https://www.acfun.cn/rest/pc-direct/rank/channel"
+	ApiSrcUrl       = "https://www.acfun.cn/?pagelets=pagelet_header,pagelet_game,pagelet_douga,pagelet_amusement,pagelet_bangumi_list,pagelet_life,pagelet_tech,pagelet_dance,pagelet_music,pagelet_film,pagelet_fishpond,pagelet_sport&reqID=0&ajaxpipe=1"
 )
 
 const (
